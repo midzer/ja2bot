00ec4583650f3ea178079b760200264b403df1b1
midzer <midzer@gmail.com>
2025-03-13 21:50:49 +0100

    new version with audio

--- index.js
+++ index.js
@@ -1,36 +1,114 @@
-import { readFileSync } from "fs"
-import axios from "axios";
+import fs from "node:fs";
+
+import { createRestAPIClient } from "masto";
 
 import configFile from "./config.json" with {type: 'json'};
+
+const masto = createRestAPIClient({
+  url: configFile.api_url,
+  accessToken: configFile.access_token,
+});
 
 function randomFrom(array) {
     return array[Math.floor(Math.random() * array.length)];
 }
 
-const file = readFileSync('ja2.txt', 'utf8');
-const quotes = file.split("\n");
+const NPCIDs =
+[
+	'BARRY',
+	'BLOOD',
+	'LYNX',
+	'GRIZZLY',
+	'VICKY',
+	'TREVOR',
+	'GRUNTY',
+	'IVAN',
+	'STEROID',
+	'IGOR',
+	'SHADOW',
+	'RED',
+	'REAPER',
+	'FIDEL',
+	'FOX',
+	'SIDNEY',
+	'GUS',
+	'BUNS',
+	'ICE',
+	'SPIDER',
+	'CLIFF',
+	'BULL',
+	'HITMAN',
+	'BUZZ',
+	'RIDER',
+	'RAVEN',
+	'STATIC',
+	'LEN',
+	'DANNY',
+	'MAGIC',
+	'STEPHEN',
+	'SCULLY',
+	'MALICE',
+	'DR_Q',
+	'NAILS',
+	'THOR',
+	'SCOPE',
+	'WOLF',
+	'MD',
+	'MELTDOWN', // 39
+	'BIFF',
+	'HAYWIRE',
+	'GASKET',
+	'RAZOR',
+	'FLO',
+	'GUMPY',
+	'LARRY_NORMAL',
+	'LARRY_DRUNK',
+	'COUGAR',
+	'NUMB',
+	'BUBBA',
+	'IMP_M0', // 51
+	'IMP_M1',
+	'IMP_M2',
+	'IMP_W0',
+	'IMP_W1',
+	'IMP_W2',
+	'MIGUEL', // 57
+	'CARLOS',
+	'IRA',
+	'DIMITRI',
+	'DEVIN',
+	'ROBOT', // 62: DUMMY 
+	'HAMOUS',
+	'SLAY',
+	'RPC65', // 65: DUMMY
+	'DYNAMO',
+	'SHANK',
+	'IGGY',
+	'VINCE',
+	'CONRAD',
+	'RPC71', // 71: DUMMY
+	'MADDOG'
+];
 
-await new Promise((resolve, reject) => {
-    const quote = randomFrom(quotes);
-    const status_message = `${quote}`;
-    console.log(`Trying to toot "${status_message}"`);
-    const config = {
-        method: 'post',
-        url: `${configFile.api_url}/statuses`,
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${configFile.access_token}`
-        },
-        data: JSON.stringify({status: status_message, language: 'en'})
-    };
+const files = fs.readdirSync('./speech/');
+const randomFile = randomFrom(files);
 
-    axios(config)
-        .then(function (res) {
-            console.log(JSON.stringify(res.data));
-            resolve(res.statusCode);
-        })
-        .catch(function (error) {
-            console.log(error);
-            reject(Error(error));
-        });
+const file = fs.readFileSync('./text/' + randomFile + '.txt', 'utf8');
+const lines = file.split('\n');
+const trimedLines = lines.map(s => s.trim());
+const quote = trimedLines.join(' ').trim();
+const face = NPCIDs[parseInt(randomFile.substring(0, 3), 10)];
+
+// Create media from a local file
+const media = await masto.v2.media.create({
+  file: new Blob([fs.readFileSync('./speech/' + randomFile)]),
+  description: quote.slice(0, 420),
+  thumbnail: new Blob([fs.readFileSync('./faces/' + face + '.png')])
+});
+
+// Publish!
+await masto.v1.statuses.create({
+  status: face + ': ' + quote,
+  visibility: "public",
+  mediaIds: [media.id],
 });
